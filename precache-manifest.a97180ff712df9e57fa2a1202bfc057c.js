c21e44c9841eb968982b0be51c587b1675b7fe2d
warbonnet333 <warbonnet333@gmail.com>
2020-02-18 14:34:05 +0200

    Updates

---
+++ precache-manifest.a97180ff712df9e57fa2a1202bfc057c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ac8e989b48be941d499ec24b243ccf22",
+    "url": "/goit-react-hw-02-phonebook/index.html"
+  },
+  {
+    "revision": "76b9715b24b32b0269f4",
+    "url": "/goit-react-hw-02-phonebook/static/css/main.e273d084.chunk.css"
+  },
+  {
+    "revision": "c6e822ea0e5941011d10",
+    "url": "/goit-react-hw-02-phonebook/static/js/2.d991fe8c.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/goit-react-hw-02-phonebook/static/js/2.d991fe8c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "76b9715b24b32b0269f4",
+    "url": "/goit-react-hw-02-phonebook/static/js/main.e5f3262b.chunk.js"
+  },
+  {
+    "revision": "d7c1b0c4902dd2644e0f",
+    "url": "/goit-react-hw-02-phonebook/static/js/runtime-main.580be532.js"
+  }
+]);
