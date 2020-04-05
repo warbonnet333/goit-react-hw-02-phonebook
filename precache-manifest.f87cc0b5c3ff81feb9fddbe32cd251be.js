68ffa0cfd07cbb7ba29c2857398ca40352950297
warbonnet333 <warbonnet333@gmail.com>
2020-04-05 14:23:59 +0300

    Updates

---
+++ precache-manifest.f87cc0b5c3ff81feb9fddbe32cd251be.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9f766d71fb5dcec0bb259c4692a37851",
+    "url": "/goit-react-hw-02-phonebook/index.html"
+  },
+  {
+    "revision": "905d8412d18ccb5b28a8",
+    "url": "/goit-react-hw-02-phonebook/static/css/main.34baa55b.chunk.css"
+  },
+  {
+    "revision": "e312cf4c6ba890ec1261",
+    "url": "/goit-react-hw-02-phonebook/static/js/2.fa9750d4.chunk.js"
+  },
+  {
+    "revision": "928d7b5eb39d16fe9a880722c974b51e",
+    "url": "/goit-react-hw-02-phonebook/static/js/2.fa9750d4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "905d8412d18ccb5b28a8",
+    "url": "/goit-react-hw-02-phonebook/static/js/main.6fa88210.chunk.js"
+  },
+  {
+    "revision": "d7c1b0c4902dd2644e0f",
+    "url": "/goit-react-hw-02-phonebook/static/js/runtime-main.580be532.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/goit-react-hw-02-phonebook/precache-manifest.6634b93223fdb5c21a36e1b0e0aa7238.js"
+  "/goit-react-hw-02-phonebook/precache-manifest.f87cc0b5c3ff81feb9fddbe32cd251be.js"
 );
 
 self.addEventListener('message', (event) => {
